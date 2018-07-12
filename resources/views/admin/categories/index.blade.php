05010d2ef163b0a5fb7cad368ad809eaacde3015
“Vovan” <vovaf@addiedigital.com>
2018-07-12 11:46:08 +0300

    Create category index page

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,56 @@
+
+
+@extends('admin.layouts.app_admin')
+
+@section('content')
+<div class="container">
+@component('admin.components.breadcrumb')
+  @slot('title') List of categories @endslot
+  @slot('parent') Main @endslot
+  @slot('active') Categories @endslot
+@endcomponent
+
+<hr>
+<a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right">
+<i class="fa fa-plus-square-o"></i>Create category
+</a>
+<table>
+  <thead>
+    <th>Name</th>
+    <th>Publication</th>
+    <th class="text-right">Action</th>
+  </thead>
+  <tbody>
+    @forelse($categories as $category)
+    <tr>
+      <td>{{$category->title}}</td>
+      <td>{{$category->published}}</td>
+      <td>
+        <a href="{{route('admin.category.edit',['id'=>$category->id])}}">
+          <i class="fa fa-edit"></i>
+        </a>
+      </td>
+    </tr>
+    <tr>
+      @empty
+      <tr>
+        <td colspan="3" class="text-center"><h2>Data absent</h2></td>
+      </tr>
+      @endforelse
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+      <td colspan="3">
+        <ul class="pagination pull-right">
+          {{$categories->link}}
+        </ul>
+      </td>
+    </tr>
+  </tfoot>
+</table>
+
+
+</div>
+
+@endsection
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,48 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+  <div class="container">
+    <div class="col-sm-3">
+      <div class="jumbotron">
+        <p><span class="label label-primary">Categories 0</span></p>
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <div class="jumbotron">
+        <p><span class="label label-primary">Materials 0</span></p>
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <div class="jumbotron">
+        <p><span class="label label-primary">Guests 0</span></p>
+      </div>
+    </div>
+    <div class="col-sm-3">
+      <div class="jumbotron">
+        <p><span class="label label-primary">Today 0</span></p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-6">
+        <a href="#" class="btn btn-block bth-default">Create cat</a>
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading">Cat 1</h4>
+          <p class="list-group-item-text">
+            Count materials
+          </p>
+        </a>
+      </div>
+      <div class="col-sm-6">
+        <a href="#" class="btn btn-block bth-default">Create material</a>
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading">Mat 1</h4>
+          <p class="list-group-item-text">
+            Category
+          </p>
+        </a>
+      </div>
+    </div>
+
+  </div>
+@endsection
